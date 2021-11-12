14b9e3bfb2ce39b17d0ed1bdf2b36ba26b8de728
Hector <hmgarcia06@gmail.com>
2021-11-12 14:31:28 -0500

    Change mock nft_mint data

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -4,14 +4,14 @@ export const nft_mint = {
     "title": "SomeNFTTitle", 
     "description": "SomeNFTDesci", 
     "media": "https://ipfs.fleek.co/ipfs/bafybeiacydivfg63rxg7idoe6xamjcvwaf4ob47kii2sgxn5hkh2pupjga",  
-    "media_hash": 0, 
-    "copies": 0, 
+    "media_hash": "", 
+    "copies": "", 
     "issued_at": "05/28/2021", 
     "expires_at": "05/28/2031", 
     "starts_at": "05/28/2021", 
     "updated_at": "what is updated_at?", 
     "extra": "SomeNFTExtra", 
     "reference": "SomeNFTReference", 
-    "reference_hash": 0
+    "reference_hash": ""
   }
 }
