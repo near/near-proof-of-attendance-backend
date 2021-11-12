435ae1d5501c30bf5b9c35060f9ef33b2c857696
Hector <hmgarcia06@gmail.com>
2021-11-12 14:30:06 -0500

    Updated typings

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -24,15 +24,15 @@ export interface TokenMetadata {
     title: string; // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
     description: string; // free-form description
     media: string; // URL to associated media; preferably to decentralized; content-addressed storage
-    media_hash: number; // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
-    copies: number; // number of copies of this set of metadata in existence when token was minted.
+    media_hash: number | string; // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
+    copies: number | string; // number of copies of this set of metadata in existence when token was minted.
     issued_at: string; // ISO 8601 datetime when token was issued or minted
     expires_at: string; // ISO 8601 datetime when token expires
     starts_at: string; // ISO 8601 datetime when token starts being valid
     updated_at: string; // ISO 8601 datetime when token was last updated
     extra: string; // anything extra the NFT wants to store on-chain. Can be stringified JSON.
     reference: string; // URL to an off-chain JSON file with more info.
-    reference_hash: number; // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
+    reference_hash: number | string; // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
 }
 
 export interface ContractConfig {
