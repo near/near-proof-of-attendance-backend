9173a39cecbb517d0cf11f73c1bb61710fdead90
RenierC <reniercuervo@gmail.com>
2021-12-09 17:23:29 -0400

    adds fix for query console error

--- server/src/__tests__/query.test.ts
+++ server/src/__tests__/query.test.ts
@@ -2,17 +2,13 @@ import request from "supertest";
 import app from "../app";
 
 const walletId = "johnq.testnet";
-const randomId = "&#%#johdkafjakjkdanq.testnet";
 
-describe("It will query the smartcontract for whitelist accounts", () => {
-  it("Should return 200 when requesting badges from an account", async () => {
-    await request(app).get(`/badges/${walletId}`).expect(200);
-  });
-
-  it("Should return an empty array when requesting badges from a random account ", async () => {
-    const connection = await request(app).get(`/badges/${randomId}`);
+describe("It will query the contract and check badges in a whitelisted account", () => {
+  it("Should return 200 when requesting badges from an account with badges", async () => {
+   const connection = await request(app).get(`/badges/${walletId}`).expect(200);
     const { body, statusCode } = connection;
     expect(statusCode).toBe(200);
-    expect(body.tokens_for_owner).toEqual([]);
+    expect(body.tokens_for_owner.length).toBeGreaterThanOrEqual(1);
   });
 });
+ 
